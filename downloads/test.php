73d029b4f2214f658b22705741a3b386a7174861
mchapman <mchapman>
2005-07-13 14:08:46 +0000

    trail version of download page

--- downloads/test.php
+++ downloads/test.php
@@ -22,7 +22,7 @@ function ListDevBuilds($dir){
    
    echo "cwd: " . getcwd() . "<br>\n";
    $f = getcdw() . "../../../download.eclipse.org/technology/ajdt/30/dev/update/changes-20050713135647.html";
-   echo "<a href=\"". $f . "\">test.html</a><br>\n";
+   echo "<a href=\"$f\">test.html</a><br>\n";
    
    $dir30="$dir/30/dev/update";
    $root=opendir($dir30) or die("Check $dir30 !");
