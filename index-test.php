e79d1bea5d7bf3b5f1259e851d6575942f69bbeb
mchapman <mchapman>
2005-12-01 11:05:54 +0000

    phoenix

--- index-test.php
+++ index-test.php
@@ -60,6 +60,78 @@
 				<a href="newfeatures/#compilerM5">New Features page</a>
 				for more details.
 			  </li>
+			  
+			  <li>
+			    <b>November 9, 2005: Load-time Weaving:</b>
+				AJDT 1.2.1 and 1.3 have added support for load-time weaving your applications.
+				Please see the <a href="newfeatures/#ltw">New Features page</a>
+				for more details.
+			  </li>
+			  
+			  <li>
+			    <b>October 31, 2005: Accessibility:</b>
+				AJDT 1.3 has undergone a full accessibility review in anticipation of the upcoming release. 
+				Please see the <a href="newfeatures/#accessibility">New Features page</a>
+				for more details.
+			  </li>
+
+			  <li>
+			    <b>October 11, 2005: AspectJ 5 M4:</b>
+				The latest AspectJ milestone release is now included in AJDT development
+				builds. Please see the <a href="newfeatures/#compilerM4">New Features page</a>
+				for more details.
+			  </li>
+
+			  <li>
+			    <b>October 3, 2005: Internationalization:</b>
+				Recently we have been focusing on improving support for message translations in AJDT. But we need
+				your help with this! Please see the <a href="newfeatures/#i18n">New Features page</a>
+				for more details.
+			  </li>
+
+			  <li>
+			    <b>September 26, 2005: Another new feature:</b>
+       			The <a href="newfeatures/#comparison">Crosscutting Comparison view</a>
+       			and related functionality helps you manage changes to the crosscutting structure
+       			of your project.
+			  </li>
+
+			  <li>
+			    <b>September 20, 2005: This week's new AJDT feature:</b>
+       			The <a href="newfeatures/#newaspect">New Aspect wizard</a> now more
+       			closely matches the appearance and
+       			functionality of the New Class wizard, plus it can generate stubs
+       			for pointcuts inherited from an abstract aspect. The version of the
+       			AspectJ compiler included in AJDT has also been updated.
+			  </li>
+
+			  <li>
+			    <b>September 12, 2005: More new features in AJDT:</b>
+       			The <a href="newfeatures/">New Features</a> page has been updated
+       			to cover the filtering capability added to the Cross References view. The version of the
+       			AspectJ compiler included in AJDT has also been updated to the latest development build.
+			  </li>
+
+			  <li>
+       			<b>September 6, 2005: New features in AJDT:</b>      
+       			Find out more about the latest new features added to AJDT
+       			development builds from the <a href="newfeatures/#pointcuts">New Features</a> page,
+       			starting this week with support for navigation and source hovers for
+       			pointcuts.			  
+			  </li>
+
+			  <li>
+			    <b>August 15, 2005: AJDT article published on developerWorks:</b> 
+       			<a target="_top" href="http://www.ibm.com/developerworks/java/library/j-aopwork9/">
+       			New AJDT releases ease AOP development</a>
+			  </li>
+
+			  <li>
+			    <b>June 10, 2005: AJDT 1.2 released for Eclipse 3.0.</b> 
+       			The stable release is now here! See the <a href="whatsnew12/">New and Noteworthy for 1.2</a>
+       			for a summary of the changes since version 1.1. The release is available from the update site
+       			and as a zip file. See the <a href="downloads/">download page</a> for more information.</td>
+			  </li>
 			</ul>
 		</div>
 		<hr class="clearer" />
