050f54e08e3b96c4c0864631b6422cd63973555c
mchapman <mchapman>
2006-01-25 14:48:50 +0000

    use new project-info dir

---
+++ description.php
@@ -0,0 +1,76 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# template.php
+	#
+	# Author: 		Denis Roy
+	# Date:			2005-06-16
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "AspectJ Development Tools (AJDT)";
+	$pageKeywords	= "AspectJ, AJDT, Aspect Oriented Software Development, Eclipse";
+	$pageAuthor		= "Matt Chapman";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+
+	$rightcolumn = file_get_contents("rightcolumn.html");
+		
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+<div id="maincontent">
+	<div id="midcolumn">
+	
+	<div align="center"><h1>$pageTitle</h1></div>
+		<table border="0" cellpadding="4">
+          <tr>
+          	<td valign="top"><img border=0 src="logo.gif" width="164" height="229" alt="AJDT screenshot"></td>
+          	<td valign="top">&nbsp;</td>
+            <td valign="top">
+
+		<p>
+		Some aspects of system implementation, such as logging, error
+		handling, standards enforcement and feature variations are
+		notoriously difficult to implement in a modular way. The result
+		is that code is tangled across a system and leads to quality,
+		productivity and maintenance problems. Aspect Oriented Software
+		Development enables the clean modularization of these crosscutting
+		concerns. The AspectJ Development Tools (AJDT) project provides
+		Eclipse platform based tool support for AOSD with AspectJ. Our goal
+		is to deliver a user experience that is consistent with the Java
+		Development Tools (JDT) when working with AspectJ projects and
+		resources. This will be accomplished by developing an integration
+		layer between the AspectJ Development Environment Framework and
+		Eclipse/JDT extension points.
+		</p>
+
+		  	</td>
+          </tr>
+
+        </table>
+			 		
+		<hr class="clearer" />
+	</div>
+
+</div>
+
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
