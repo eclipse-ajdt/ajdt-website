7a6535f6908a1e4b5c933d550ac48553861cbbe9
mchapman <mchapman>
2006-01-25 14:06:24 +0000

    use new project-info dir

--- _projectCommon.php
+++ _projectCommon.php
@@ -12,9 +12,9 @@
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	$Nav->addNavSeparator("AJDT Project", 	"/ajdt/");
-	$Nav->addCustomNav("Downloads", 	"/ajdt/downloads/", 	"_self", 1);
+	#$Nav->addCustomNav("Downloads", 	"/ajdt/downloads/", 	"_self", 1);
 	$Nav->addCustomNav("New Features", 	"/ajdt/newfeatures/", "_self", 1);
-	$Nav->addCustomNav("Demos", 		"/ajdt/demos/", 		"_self", 1);
+	#$Nav->addCustomNav("Demos", 		"/ajdt/demos/", 		"_self", 1);
 	$Nav->addCustomNav("FAQ", 			"/ajdt/faq.php", 	"_self", 1);
 
 ?>
---
+++ project-info/committers.php
@@ -0,0 +1,111 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# template.php
+	#
+	# Author: 		Denis Roy
+	# Date:			2005-06-16
+	#
+	# Description: Type your page comments here - these are not sent to the browser
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "AJDT Contributors";
+	$pageKeywords	= "AspectJ, AJDT, Aspect Oriented Software Development, Eclipse, Contributors";
+	$pageAuthor		= "Matt Chapman";
+	
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	$rightcolumn = file_get_contents("rightcolumn.html");
+		
+	# Paste your HTML content between the EOHTML markers!	
+	$html = <<<EOHTML
+
+<div id="maincontent">
+	<div id="midcolumn">
+	
+	<div align="center"><h1>$pageTitle</h1></div>
+	
+	<p> Below is the list of core committers for the AJDT project.
+	We are also grateful to number of <a href="contributors.php">contributors.</a>
+	</p>
+
+	<div class="homeitem">
+		<h3>Committers</h3>
+			<table cellpadding="2">
+<tr>
+        <td>Matt Chapman
+	    </td>
+        <td>
+          IBM UK Ltd
+	    </td>
+      </tr>
+      <tr>
+        <td>Andy Clement
+	    </td>
+        <td>
+          IBM UK Ltd
+	    </td>
+      </tr>
+      <tr>
+        <td>Adrian Colyer
+	    </td>
+        <td>
+          IBM UK Ltd
+	    </td>
+      </tr>
+      <tr>
+        <td>Helen Hawkins
+	    </td>
+        <td>
+          IBM UK Ltd
+	    </td>
+      </tr>
+      <tr>
+        <td>Sian January
+	    </td>
+        <td>
+          IBM UK Ltd
+	    </td>
+      </tr>
+      <tr>
+        <td>
+		<a href="http://kerstens.org/mik">Mik Kersten</a>
+	    </td>
+        <td>
+          PARC
+	    </td>
+      </tr>
+      <tr>
+        <td>Julie Waterhouse
+	    </td>
+        <td>
+          IBM Canada
+	    </td>
+      </tr>			
+			</table>
+	</div>
+	
+		<hr class="clearer" />
+	</div>
+
+</div>
+
+
+EOHTML;
+
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
