6ab7e457ca71c2e0e323c15f0e0451be644a3f3d
mchapman <mchapman>
2006-01-30 10:56:28 +0000

    new nav layout

--- _projectCommon.php
+++ _projectCommon.php
@@ -6,15 +6,21 @@
 	$theme = "Phoenix";
 
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/projects/common/project-info.class.php");
-	$projectInfo = new ProjectInfo("technology.ajdt");
-	$projectInfo->generate_common_nav( $Nav );
+	#$projectInfo = new ProjectInfo("technology.ajdt");
+	#$projectInfo->generate_common_nav( $Nav );
 
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	#$Nav->addNavSeparator("AJDT Project", 	"/ajdt/");
-	#$Nav->addCustomNav("Downloads", 	"/ajdt/downloads/", 	"_self", 1);
-	$Nav->addCustomNav("New Features", 	"/ajdt/newfeatures/", "_self", 1);
-	$Nav->addCustomNav("Demos", 		"/ajdt/demos/", 		"_self", 1);
-	#$Nav->addCustomNav("FAQ", 			"/ajdt/faq.php", 	"_self", 1);
+	$Nav->addNavSeparator("AJDT", 	"/ajdt/");
+	$Nav->addCustomNav("About", 	"/ajdt/about.php/", 	"_self", 1);
+
+	$Nav->addCustomNav("Users", 	"/ajdt/users.php/", 	"_self", 1);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;Getting Started", 	"/ajdt/users.php/", 	"_self", 1);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;Downloads", 	"/ajdt/downloads/", 	"_self", 1);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;New Features", 	"/ajdt/newfeatures/", "_self", 1);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;Demos", 		"/ajdt/demos/", 		"_self", 1);
+	$Nav->addCustomNav("&nbsp;&nbsp;&nbsp;FAQ", 			"/ajdt/faq.php", 	"_self", 1);
+
+	$Nav->addCustomNav("Developers", 	"/ajdt/developers.php/", 	"_self", 1);
 
 ?>
