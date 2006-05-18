9f3ad9c8ac103d9225266e44248043d5e307be01
hawkins <hawkins>
2006-05-18 13:02:30 +0000

    updating to include new drop of aj

--- newfeatures/index.php
+++ newfeatures/index.php
@@ -51,6 +51,21 @@ or for further discussion or suggestions please visit the
 		<ul>
 
 		<li>
+			<a name="compiler20060518">Updated AspectJ Compiler</a>	
+			<span class="dates">(posted 18-05-06)</span>
+		<p>
+           The AspectJ build included in AJDT has been updated in development build
+           <b>1.4.0.20060518082333 for Eclipse 3.2RC4</b>
+        </p>
+        <p>
+           The timestamp of the included AspectJ 5 build is 20060518084747.
+           See the <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=AspectJ&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=RESOLVED&resolution=FIXED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=2006-05-12&chfieldto=2006-05-18&chfield=bug_status&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=&field0-1-0=noop&type0-1-0=noop&value0-1-0=&field0-1-1=noop&type0-1-1=noop&value0-1-1=&field1-0-0=noop&type1-0-0=noop&value1-0-0=">
+           list of AspectJ bugs fixed in this build since the last compiler update.</a>
+        </p>        
+  
+		</li>	
+
+		<li>
 			<a name="compiler20060512">Updated AspectJ Compiler</a>	
 			<span class="dates">(posted 12-05-06)</span>
 		<p>
