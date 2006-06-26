47153745c710f9e3235cfa72ebdc6494a8291bb7
mchapman <mchapman>
2006-06-26 14:56:47 +0000

    3.2

--- downloads/index.php
+++ downloads/index.php
@@ -41,7 +41,7 @@ function ListDevBuilds($dir){
 		 } else if ($eclipse == "31") {
 		   $eclipsename = "3.1";
 		 } else {
-		   $eclipsename = "3.2RC7";
+		   $eclipsename = "3.2";
 		 }
          preg_match('/.*ajdt_(.*)_archive.zip/',$file, $matches);
 		 $name = $matches[1] . " for Eclipse " . $eclipsename;
