ff952a8d892070a0bb6fee2e605c0711f8e16116
mchapman <mchapman>
2006-10-09 10:51:13 +0000

    update tasks page

--- tasks.php
+++ tasks.php
@@ -62,6 +62,131 @@
 </tr>
 
 <tr>
+<td><h4>AspectJ Search</h4>
+Support searching for AspectJ constructs within the JDT search dialog.
+</td>
+<td>Not yet assigned (<a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=71073">bug&nbsp;71073</a>)</td>
+<td>Not yet assigned</td>
+<td>Not yet planned</td>
+</tr>
+
+<tr>
+<td><h4>Content Assist (Ctrl + space)</h4>
+Content assist now works well in many cases. This is implemented by getting
+JDT to provide completions for modified versions of the buffer. We're close
+to the limits of this approach - for full aspect aware code assist, e.g.
+completions on pointcut names and parameters following the beginning of an
+advice statement, we need our own completion processor.
+</td>
+<td>Not yet assigned</td>
+<td>Not yet assigned</td>
+<td>Not yet planned</td>
+</tr>
+
+<tr>
+<td><h4>Tooltip descriptions (F2)</h4>
+For AspectJ keywords show an expanded version of the AspectJ Quick
+Reference summary for that construct. For AspectJ declarations, show the
+javadoc (ajdoc) comment and signature for those declarations.
+</td>
+<td>Not yet assigned</td>
+<td>Not yet assigned</td>
+<td>Not yet planned</td>
+</tr>
+
+<tr>
+<td><h4>Parameter Hints (Ctrl + shift + space)</h4>
+For pointcuts, shows the possible matching points – e.g. call([visibility]
+return_type TypePattern.MethodNamePattern( ArgsPattern ).
+To be investigated to see whether this would be useful or not.
+</td>
+<td>Not yet assigned</td>
+<td>Not yet assigned</td>
+<td>Not yet planned</td>
+</tr>
+
+<tr>
+<td><h4>Java Refactoring Support</h4>
+Ensure that existing
+refactorings in Eclipse work correctly in the presence of aspects.
+</td>
+<td>Started with Rename participation (<a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=158954">bug&nbsp;158954</a>)</td>
+<td>Matt Chapman</td>
+<td>1.4.1</td>
+</tr>
+
+<tr>
+<td><h4>AspectJ Refactoring Support</h4>
+Add aspect-specific refactorings.
+</td>
+<td>Not yet assigned</td>
+<td>Not yet assigned</td>
+<td>Not yet planned</td>
+</tr>
+
+<tr>
+<td><h4>Complete debugging support</h4>
+The remaining work in this area is to tidy up the stack, to avoid
+exposing internal compiler generated names.
+</td>
+<td>Not yet assigned</td>
+<td>Not yet assigned</td>
+<td>Not yet planned</td>
+</tr>
+
+<tr>
+<td><h4>Pointcut Matcher</h4>
+This view would allow you to enter a (static?) pointcut expression – either
+by directly typing or by using the pointcut wizard and see the matches in
+a search results list and also in a visualiser like view. Optionally may
+support naming and saving of queries.
+</td>
+<td>In progress</td>
+<td>Julie&nbsp;Waterhouse, Mik&nbsp;Kersten</td>
+<td>Not yet planned</td>
+</tr>
+
+<tr>
+<td><h4>Pointcut Wizard</h4>
+The pointcut wizard guides you step by step through the process of creating
+a pointcut that does what you want. It encourages good style, teaches an AO
+way of thinking about pointcuts, and has knowledge of common idioms.
+</td>
+<td>Not yet assigned</td>
+<td>Not yet assigned</td>
+<td>Not yet planned</td>
+</tr>
+
+<tr>
+<td><h4>Pointcut Reader</h4>
+This is a sophisticated form of tooltip for a pointcut declaration
+which “reads” the pointcut in plain English.
+</td>
+<td>Not yet assigned</td>
+<td>Not yet assigned</td>
+<td>Not yet planned</td>
+</tr>
+
+			</table>
+			<br></a></div>
+	</div>
+
+
+
+
+
+	<div class="homeitem3col">
+		<h3>Recently Completed Tasks</h3>
+			<div align="center">
+			<table border="1" cellpadding="2">
+<tr>
+<th>Description of task</th>
+<th>Status</th>
+<th>Owner</th>
+<th>Release</th>
+</tr>
+
+<tr>
 <td><h4>Launch support for Load Time Weaving</h4>
 We now have AspectJ run configuration support. We need to determine
 what to add to this to support load time weaving.
@@ -135,105 +260,6 @@ nice to get rid of it if we can).
 </tr>
 
 <tr>
-<td><h4>AspectJ Search</h4>
-Support searching for AspectJ constructs within the JDT search dialog.
-</td>
-<td>Not yet assigned (<a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=71073">bug&nbsp;71073</a>)</td>
-<td>Not yet assigned</td>
-<td>Not yet planned</td>
-</tr>
-
-<tr>
-<td><h4>Content Assist (Ctrl + space)</h4>
-Content assist now works well in many cases. This is implemented by getting
-JDT to provide completions for modified versions of the buffer. We're close
-to the limits of this approach - for full aspect aware code assist, e.g.
-completions on pointcut names and parameters following the beginning of an
-advice statement, we need our own completion processor.
-</td>
-<td>Not yet assigned</td>
-<td>Not yet assigned</td>
-<td>Not yet planned</td>
-</tr>
-
-<tr>
-<td><h4>Tooltip descriptions (F2)</h4>
-For AspectJ keywords show an expanded version of the AspectJ Quick
-Reference summary for that construct. For AspectJ declarations, show the
-javadoc (ajdoc) comment and signature for those declarations.
-</td>
-<td>Not yet assigned</td>
-<td>Not yet assigned</td>
-<td>Not yet planned</td>
-</tr>
-
-<tr>
-<td><h4>Parameter Hints (Ctrl + shift + space)</h4>
-For pointcuts, shows the possible matching points – e.g. call([visibility]
-return_type TypePattern.MethodNamePattern( ArgsPattern ).
-To be investigated to see whether this would be useful or not.
-</td>
-<td>Not yet assigned</td>
-<td>Not yet assigned</td>
-<td>Not yet planned</td>
-</tr>
-
-<tr>
-<td><h4>Refactoring Support</h4>
-There are two pieces to this: the first is to ensure that existing
-refactorings in Eclipse work correctly in the presence of aspects; the
-second is to add aspect-specific refactorings. There are several research
-groups looking into these problems.
-</td>
-<td>Not yet assigned</td>
-<td>Not yet assigned</td>
-<td>Not yet planned</td>
-</tr>
-
-<tr>
-<td><h4>Complete debugging support</h4>
-The remaining work in this area is to tidy up the stack, to avoid
-exposing internal compiler generated names.
-</td>
-<td>Not yet assigned</td>
-<td>Not yet assigned</td>
-<td>Not yet planned</td>
-</tr>
-
-<tr>
-<td><h4>Pointcut Matcher</h4>
-This view would allow you to enter a (static?) pointcut expression – either
-by directly typing or by using the pointcut wizard and see the matches in
-a search results list and also in a visualiser like view. Optionally may
-support naming and saving of queries.
-</td>
-<td>In progress</td>
-<td>Julie&nbsp;Waterhouse, Mik&nbsp;Kersten</td>
-<td>Not yet planned</td>
-</tr>
-
-<tr>
-<td><h4>Pointcut Wizard</h4>
-The pointcut wizard guides you step by step through the process of creating
-a pointcut that does what you want. It encourages good style, teaches an AO
-way of thinking about pointcuts, and has knowledge of common idioms.
-</td>
-<td>Not yet assigned</td>
-<td>Not yet assigned</td>
-<td>Not yet planned</td>
-</tr>
-
-<tr>
-<td><h4>Pointcut Reader</h4>
-This is a sophisticated form of tooltip for a pointcut declaration
-which “reads” the pointcut in plain English.
-</td>
-<td>Not yet assigned</td>
-<td>Not yet assigned</td>
-<td>Not yet planned</td>
-</tr>
-
-<tr>
 <td><h4>Aspect Refactoring Verification</h4>
 Compares a query (for example “call(void Canvas.updateHistory(..))” ) against
 an aspect supplying advice (for example, “after() returning:
@@ -250,13 +276,14 @@ vice-versa. Very useful support as part of the “Extract to Aspect” refactoring.
 <td><h4>AspectJ AST</h4>
 Make the AspectJ structure model easily available to extenders
 </td>
-<td>Assigned (<a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=88861">bug&nbsp;88861</a>)</td>
+<td>Completed (<a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=88861">bug&nbsp;88861</a>)</td>
 <td>Andy Clement</td>
-<td>Not yet planned</td>
+<td>1.4</td>
 </tr>
 			</table>
 			<br></a></div>
 	</div>
+
 
 		<hr class="clearer" />
 	</div>
