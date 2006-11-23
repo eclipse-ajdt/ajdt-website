a5f12b8912640c4c32d568b63099cd16b51a121f
mchapman <mchapman>
2006-11-23 12:41:09 +0000

    GO for ajdt 1.3.3 & 1.2.3

--- downloads/index.php
+++ downloads/index.php
@@ -193,6 +193,30 @@ of /technolgy, since AJDT become a Tools project.</p>
 	<ul>
 
 		<li>
+			<a>AJDT 1.3.3 for Eclipse 3.1</a>
+		
+<table class="tasklist" width="80%">
+  <tr>
+    <th align="left">Release Date:</th><td>November 23, 2006</td>
+  </tr>
+  <tr>
+    <th align="left">AspectJ Version:</th><td>1.5.3</td>
+  </tr>
+  <tr>
+    <th align="left">Update&nbsp;Site&nbsp;URL:</th>
+    <td><code>http://download.eclipse.org/tools/ajdt/31/update</code></td>
+  </tr>
+  <tr>
+    <th align="left">Zip file:</th>
+    <td><a href="http://www.eclipse.org/downloads/download.php?file=/tools/ajdt/31/update/ajdt_1.3.3_for_eclipse_3.1.zip">ajdt_1.3.3_for_eclipse_3.1.zip</a></td>
+  </tr>
+  <tr>
+    <th align="left">More info:</th><td>The only change since 1.3.2 is an update to the latest AspectJ release.</td>
+  </tr>
+</table>
+		</li>
+
+		<li>
 			<a>AJDT 1.3.2 for Eclipse 3.1</a>
 		
 <table class="tasklist" width="80%">
@@ -223,6 +247,31 @@ of /technolgy, since AJDT become a Tools project.</p>
 	<div class="homeitem3col">
 	<h3>Release builds for Eclipse 3.0</h3>
 	<ul>
+
+		<li>
+			<a>AJDT 1.2.3 for Eclipse 3.0</a>
+		
+<table class="tasklist" width="80%">
+  <tr>
+    <th align="left">Release Date:</th><td>November 23, 2006</td>
+  </tr>
+    <tr>
+    <th align="left">AspectJ Version:</th><td>1.5.3</td>
+  </tr>
+  <tr>
+    <th align="left">Update&nbsp;Site&nbsp;URL:</th>
+    <td><code>http://download.eclipse.org/tools/ajdt/30/update</code></td>
+  </tr>
+  <tr>
+    <th align="left">Zip file:</th>
+    <td><a href="http://www.eclipse.org/downloads/download.php?file=/tools/ajdt/30/update/ajdt_1.2.3_for_eclipse_3.0.zip">ajdt_1.2.3_for_eclipse_3.0.zip</a></td>
+  </tr>
+  <tr>
+    <th align="left">More info:</th><td>The only change since 1.2.2 is an update to the latest AspectJ release.</td>
+  </tr>
+</table>
+
+		</li>
 
 		<li>
 			<a>AJDT 1.2.2 for Eclipse 3.0</a>
