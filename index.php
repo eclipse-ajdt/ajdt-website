24b29d0931ee921e5d47b2b708d47c50d9e60dde
mchapman <mchapman>
2007-03-15 14:37:07 +0000

    new feature: crosscutting changed markers

--- index.php
+++ index.php
@@ -112,7 +112,6 @@
 			<ul>
 
 			  <li>
-			  	<img src="images/new.gif" width="11" height="11">
       			<a href="downloads/#milestone">AJDT 1.5M5 for Eclipse 3.3M5 released!</a>
 				<br><span class="dates">posted 16-02-07</span>
 			  </li>
