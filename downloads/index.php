03b4573794028b7248ee9c890e3fadca7726a778
mchapman <mchapman>
2007-03-30 14:23:56 +0000

    3.3M6

--- downloads/index.php
+++ downloads/index.php
@@ -276,6 +276,31 @@ of /technology, since AJDT become a Tools project.</p>
 	<ul>
 
 		<li>
+			<a>AJDT 1.5M6 for Eclipse 3.3M6</a>
+		
+<table class="tasklist" width="80%">
+  <tr>
+    <th align="left">Release Date:</th><td>March 30, 2007</td>
+  </tr>
+    <tr>
+    <th align="left">AspectJ Version:</th><td>1.5.4.200701151324</td>
+  </tr>
+  <tr>
+    <th align="left">Update&nbsp;Site&nbsp;URL:</th>
+    <td><code>http://download.eclipse.org/tools/ajdt/33/update</code></td>
+  </tr>
+  <tr>
+    <th align="left">Zip file:</th>
+    <td><a href="http://www.eclipse.org/downloads/download.php?file=/tools/ajdt/33/update/ajdt_1.5M6_for_eclipse_3.3M6.zip">ajdt_1.5M6_for_eclipse_3.3M6.zip</a></td>
+  </tr>
+  <tr>
+    <th align="left">More info:</th><td>See the <a href="http://www.eclipse.org/ajdt/newfeatures/">New Features</a> page</td>
+  </tr>
+</table>
+
+		</li>
+
+		<li>
 			<a>AJDT 1.5M5 for Eclipse 3.3M5</a>
 		
 <table class="tasklist" width="80%">
