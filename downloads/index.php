ecc58b7e977a63cf0ef84c8642c9bb4c4743ca44
aclement <aclement>
2008-07-07 15:41:48 +0000

    for Ben - made the download obvious

--- downloads/index.php
+++ downloads/index.php
@@ -164,7 +164,7 @@ of Eclipse you are using.</p>
 <p>Note also that the Update Sites are now under /tools instead
 of /technology, since AJDT become a Tools project.</p>
 	<div class="homeitem3col">
-	<h3>Development builds for Eclipse 3.4</h3>
+	<h3>Development builds for Eclipse 3.3</h3>
 	<ul>
 
 		<li>
